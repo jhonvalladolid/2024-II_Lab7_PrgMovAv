e4398097604c3c96cd8a201685d84dc4bea5e0c2
jhonvalladolid <jhonvalladolidoficial@gmail.com>
2024-10-03 22:00:04 -0500

    Interface realizado con funciones de suma y validacion de entradas

--- UsodeControlesValladolid/FirstViewController.swift
+++ UsodeControlesValladolid/FirstViewController.swift
@@ -9,12 +9,58 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var txtRpta: UITextField!
+    @IBOutlet weak var txtNumero1: UITextField!
+    @IBOutlet weak var txtNumero2: UITextField!
+    
+    var nro1:Double = 0
+    var nro2:Double = 0
+    var rpta:Double = 0
+    
+    @IBAction func btnSumar(_ sender: Any) {
+        if Double(txtNumero1.text!) != nil && Double(txtNumero2.text!) != nil {
+            nro1 = Double(txtNumero1.text!)!
+            nro2 = Double(txtNumero2.text!)!
+            rpta = nro1 + nro2
+            txtRpta.text = String(rpta)
+        } else {
+            mostrarAlerta(titulo: "Error", mensaje: "Debe introducir solo valores numéricos")
+            print("Error al realizar operación")
+        }
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let tap:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ocultarTeclado))
+        view.addGestureRecognizer(tap)
+
+    }
+    
+    func Limpiar() {
+        self.txtNumero1.text = "0"
+        self.txtNumero2.text = "0"
+        self.txtRpta.text = "0"
+    }
+    
+    func mostrarAlerta(titulo: String, mensaje: String) {
+        let alerta = UIAlertController(title: titulo, message: mensaje, preferredStyle: .alert)
+        let limpiar = UIAlertAction(title: "Limpiar", style: .default) { (action) in
+            self.Limpiar()
+        }
+        
+        let cancelar = UIAlertAction(title: "Cancelar", style: .cancel, handler: {(action) in
+            
+        })
+        
+        alerta.addAction(limpiar)
+        alerta.addAction(cancelar)
+        
+        present(alerta, animated: true, completion: nil)
+    }
+    
+    @objc func ocultarTeclado() {
+        view.endEditing(true)
+    }
 
     /*
     // MARK: - Navigation
